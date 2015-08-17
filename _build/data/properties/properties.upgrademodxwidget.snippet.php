b03c435069197ad9ecd48f209867c684c4061dea
bobray <bobray@softville.com>
2015-08-17 02:39:25 -0500

    Add snippet properties

---
+++ _build/data/properties/properties.upgrademodxwidget.snippet.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Properties file for UpgradeModxWidget snippet
+ *
+ * Copyright 2015 by Bob Ray <http://bobsguides.com>
+ * Created on 08-17-2015
+ *
+ * @package upgrademodx
+ * @subpackage build
+ */
+
+
+
+
+$properties = array (
+  'interval' => 
+  array (
+    'name' => 'interval',
+    'desc' => '',
+    'type' => 'textfield',
+    'options' => 
+    array (
+    ),
+    'value' => '60 seconds',
+    'lexicon' => NULL,
+    'area' => '',
+  ),
+  'lastCheck' => 
+  array (
+    'name' => 'lastCheck',
+    'desc' => '',
+    'type' => 'textfield',
+    'options' => 
+    array (
+    ),
+    'value' => '2015-08-17 00:13:44',
+    'lexicon' => NULL,
+    'area' => '',
+  ),
+  'plOnly' => 
+  array (
+    'name' => 'plOnly',
+    'desc' => '',
+    'type' => 'textfield',
+    'options' => 
+    array (
+    ),
+    'value' => true,
+    'lexicon' => NULL,
+    'area' => '',
+  ),
+  'hideWhenNoUpdate' => 
+  array (
+    'name' => 'hideWhenNoUpdate',
+    'desc' => '',
+    'type' => 'textfield',
+    'options' => 
+    array (
+    ),
+    'value' => false,
+    'lexicon' => NULL,
+    'area' => '',
+  ),
+);
+
+return $properties;
+
