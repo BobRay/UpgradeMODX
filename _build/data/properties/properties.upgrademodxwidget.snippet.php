09a9e72da305ff3e17b5c246dfa092517085de9d
bobray <bobray@softville.com>
2015-08-18 01:27:07 -0500

    Added latestVersion property to store latest version

--- _build/data/properties/properties.upgrademodxwidget.snippet.php
+++ _build/data/properties/properties.upgrademodxwidget.snippet.php
@@ -13,54 +13,77 @@
 
 
 $properties = array (
-  'interval' => 
+  'VersionsToShow' => 
   array (
-    'name' => 'interval',
-    'desc' => '',
+    'name' => 'versionsToShow',
+    'desc' => 'Number of versions to show in upgrade form (not widget); default: 5',
     'type' => 'textfield',
     'options' => 
     array (
     ),
-    'value' => '60 seconds',
-    'lexicon' => NULL,
-    'area' => '',
-  ),
-  'lastCheck' => 
-  array (
-    'name' => 'lastCheck',
-    'desc' => '',
-    'type' => 'textfield',
-    'options' => 
-    array (
-    ),
-    'value' => '2015-08-17 00:13:44',
-    'lexicon' => NULL,
-    'area' => '',
-  ),
-  'plOnly' => 
-  array (
-    'name' => 'plOnly',
-    'desc' => '',
-    'type' => 'textfield',
-    'options' => 
-    array (
-    ),
-    'value' => true,
-    'lexicon' => NULL,
+    'value' => '5',
+    'lexicon' => 'upgrademodx:default',
     'area' => '',
   ),
   'hideWhenNoUpdate' => 
   array (
     'name' => 'hideWhenNoUpdate',
-    'desc' => '',
-    'type' => 'textfield',
+    'desc' => 'Hide widget when no update is available: default: No',
+    'type' => 'combo-boolean',
     'options' => 
     array (
     ),
     'value' => false,
-    'lexicon' => NULL,
+    'lexicon' => 'upgrademodx:default',
     'area' => '',
   ),
+  'interval' => 
+  array (
+    'name' => 'interval',
+    'desc' => 'Interval between checks -- Examples: 1 week, 3 days, 6 hours; default: 1 week',
+    'type' => 'textfield',
+    'options' => 
+    array (
+    ),
+    'value' => '60 seconds',
+    'lexicon' => 'upgrademodx:default',
+    'area' => '',
+  ),
+
+  'plOnly' => 
+  array (
+    'name' => 'plOnly',
+    'desc' => 'Show only pl (stable) versions; default: yes',
+    'type' => 'combo-boolean',
+    'options' => 
+    array (
+    ),
+    'value' => false,
+    'lexicon' => 'upgrademodx:default',
+    'area' => '',
+  ),
+  'lastCheck' =>
+  array(
+  'name' => 'lastCheck',
+          'desc' => 'Date and time of last check -- set automatically',
+          'type' => 'textfield',
+          'options' =>
+              array(),
+          'value' => '2015-08-17 00:13:44',
+          'lexicon' => 'upgrademodx:default',
+          'area' => '',
+      ),
+    'latestVersion' =>
+        array(
+            'name' => 'latestVersion',
+            'desc' => 'Latest version (at last check) -- set automatically',
+            'type' => 'textfield',
+            'options' =>
+                array(),
+            'value' => '',
+            'lexicon' => 'upgrademodx:default',
+            'area' => '',
+        ),
 );
 
 return $properties;
