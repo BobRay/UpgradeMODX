26b6e344f189b61f9aaa115a20e9f5cf199bc928
bobray <bobray@softville.com>
2015-08-20 02:43:02 -0500

    Added properties comment

--- core/components/upgrademodx/elements/snippets/upgrademodxwidget.snippet.php
+++ core/components/upgrademodx/elements/snippets/upgrademodxwidget.snippet.php
@@ -33,6 +33,18 @@
  *
  * @package upgrademodx
  **/
+
+/* Properties
+
+ * @property &groups textfield -- group, or commma-separated list of groups, who will see the widget; Default: (empty)..
+ * @property &hideWhenNoUpgrade combo-boolean -- Hide widget when no upgrade is available; Default: No.
+ * @property &interval textfield -- Interval between checks -- Examples: 1 week, 3 days, 6 hours; Default: 1 week.
+ * @property &lastCheck textfield -- Date and time of last check -- set automatically; Default: (empty)..
+ * @property &latestVersion textfield -- Latest version (at last check) -- set automatically; Default: (empty)..
+ * @property &plOnly combo-boolean -- Show only pl (stable) versions; Default: yes.
+ * @property &versionsToShow textfield -- Number of versions to show in upgrade form (not widget); Default: 5.
+
+ */
 class UpgradeMODX
 {
 
