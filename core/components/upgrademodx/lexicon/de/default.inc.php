57b2b918dc2ab754f59690ab2b3dea1ce3a6e141
Sebastian G. Marinescu <sebastian-marinescu@users.noreply.github.com>
2016-08-18 18:10:33 +0200

    German Default Topic Lexicon translation

---
+++ core/components/upgrademodx/lexicon/de/default.inc.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * de default topic lexicon file for UpgradeMODX extra
+ *
+ * Copyright 2015 by Bob Ray <http://bobsguides.com>
+ *
+ * German Translation by Sebastian G. Marinescu
+ * Created on 08-18-2016
+ *
+ * UpgradeMODX is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * UpgradeMODX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * UpgradeMODX; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package upgrademodx
+ */
+
+/**
+ * Description
+ * -----------
+ * de default topic lexicon strings
+ *
+ * Variables
+ * ---------
+ * @var $modx modX
+ * @var $scriptProperties array
+ *
+ * @package upgrademodx
+ **/
+
+$_lang['package'] = 'UpgradeModx';
+
+/* Used in upgrademodxwidget.snippet.php */
+$_lang['ugm_current_version_caption'] = 'Aktuelle Version';
+$_lang['ugm_latest_version_caption'] = 'Neueste Version';
+$_lang['ugm_empty_return'] = 'Leere Antwort';
+$_lang['ugm_no_version_list'] = 'Konnte keine Versions-Liste empfangen';
+$_lang['ugm_could_not_open'] = 'Konnte nicht geöffnet werden';
+$_lang['ugm_for_writing'] = 'zum Schreiben';
+$_lang['ugm_missing_properties'] = 'lastCheck oder interval Eigenschaften nicht gesetzt';
+$_lang['ugm_upgrade_available'] = 'Update verfügbar';
+$_lang['ugm_modx_up_to_date'] = 'MODX ist aktuell';
+$_lang['ugm_error'] = 'Fehler';
+$_lang['ugm_logout_note'] = 'Hinweis: Alle Nutzer werden abgemeldet';
+$_lang['ugm_upgrade_modx'] = 'Aktualisieren Sie MODX';
+$_lang['ugm_json_decode_failed'] = 'JSON decode für Versionsdaten von Github in upgradeAvailable() fehlgeschlagen';
+$_lang['ugm_not_available'] = 'Neue Version steht noch nicht im MODX-Verzeichnis zum Download bereit';
+$_lang['ugm_no_curl_no_fopen'] = 'Weder allow_url_fopen noch cURL können zur Überprüfung von Updates verwendet werden';
+
+/* Used in upgrademodx.class.php */
+$_lang['failed'] = 'fehlgeschlagen';
