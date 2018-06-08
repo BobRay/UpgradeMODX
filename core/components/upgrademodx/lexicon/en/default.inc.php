d3e6dced8203310fd1492abce31a6ed0ed4caa6e
bobray <bobray@softville.com>
2018-06-08 14:12:08 -0500

    Update some lex strings

--- core/components/upgrademodx/lexicon/en/default.inc.php
+++ core/components/upgrademodx/lexicon/en/default.inc.php
@@ -54,5 +54,17 @@ $_lang['ugm_json_decode_failed'] = 'Failed JSON decode for version data from Git
 $_lang['ugm_not_available'] = 'New version not yet available for download at MODX repo';
 $_lang['ugm_no_curl_no_fopen'] = 'Neither allow_url_fopen nor cURL can be used to check for upgrades';
 $_lang['ugm_no_such_version'] = 'Requested version does not exist';
+
+
+/* Used in upgrademodxscriptsource chunk to tell when process
+ *  is finished and Setup should be launched!!!!*/
+$_lang['Launching Setup'] = 'Launching Setup';
+$_lang['Finished'] = 'Finished';
+
+/* Used in upgrademodxscriptsource to report Progress */
+
+
+
+
 /* Used in upgrademodx.class.php */
 $_lang['failed'] = 'failed';
