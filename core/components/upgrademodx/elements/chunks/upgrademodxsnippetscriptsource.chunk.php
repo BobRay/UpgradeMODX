5750afac442f3d71d7943b517982cfbbdd09f481
bobray <bobray@softville.com>
2018-06-18 00:34:14 -0500

    Add polyfill for IE

--- core/components/upgrademodx/elements/chunks/upgrademodxsnippetscriptsource.chunk.php
+++ core/components/upgrademodx/elements/chunks/upgrademodxsnippetscriptsource.chunk.php
@@ -447,6 +447,21 @@ if ($submitted) {
     <script>
         var previous = "Begin Upgrade";
         var url = "[[+ugm_progress_url]]";
+        
+        /* Polyfill for IE */
+        if (!String.prototype.includes) {
+             String.prototype.includes = function(search, start) {
+                if (typeof start !== 'number') {
+                    start = 0;
+                }
+
+                if (start + search.length > this.length) {
+                    return false;
+                } else {
+                    return this.indexOf(search, start) !== -1;
+                }
+          };
+        }
              
         poll();            
           
