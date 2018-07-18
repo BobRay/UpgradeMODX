2269402f8e20b2e68e46a5b2e2b2a43e159205c0
bobray <bobray@softville.com>
2018-07-18 01:33:41 -0500

    Add skeleton processors

---
+++ core/components/upgrademodx/processors/unzipfiles.class.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Processor file for UpgradeMODX extra
+ *
+ * Copyright 2015-2018 by Bob Ray <https://bobsguides.com>
+ * Created on 07-16-2018
+ *
+ * UpgradeMODX is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * UpgradeMODX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * UpgradeMODX; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package upgrademodx
+ * @subpackage processors
+ */
+
+/* @var $modx modX */
+
+
+class UpgradeMODXUnzipfilesProcessor extends modProcessor {
+    public $classKey = 'mod';
+    public $languageTopics = array('upgrademodx:default');
+    public $defaultSortField = 'name';
+    public $defaultSortDirection = 'ASC';
+    public $ids;
+
+    function initialize() {
+        /* Initialization here */
+        return true;
+    }
+
+    /* For built-in processors (create, update, duplicate, remove),
+       this method can be removed */
+    public function process() {
+
+        /* perform action here */
+
+        return $this->success();
+
+    }
+}
+
+return 'UpgradeMODXUnzipfilesProcessor';
