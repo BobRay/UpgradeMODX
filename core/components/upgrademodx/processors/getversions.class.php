d413512979425b88e1aeed863fa652b321bf1fd9
bobray <bobray@softville.com>
2018-08-06 22:42:22 -0500

    Add error handling for getVersions

--- core/components/upgrademodx/processors/getversions.class.php
+++ core/components/upgrademodx/processors/getversions.class.php
@@ -27,8 +27,9 @@ use GuzzleHttp\Client;
 
 /* @var $modx modX */
 
+include_once ('ugmprocessor.class.php');
 
-class GetVersionsProcessor extends modProcessor {
+class GetVersionsProcessor extends UgmProcessor {
 
     public $languageTopics = array('upgrademodx:default');
     public $corePath;
@@ -37,6 +38,7 @@ class GetVersionsProcessor extends modProcessor {
     public $method = 'curl';
     protected $username = '';
     protected $token = '';
+    /** @var $client GuzzleHttp\Client */
     protected $client = null;
 
     function initialize() {
@@ -56,58 +58,67 @@ class GetVersionsProcessor extends modProcessor {
     }
 
     public function getJSONFromGitHub($method) {
-        // $header = "Authorization: Basic " . base64_encode($username . ':' . $token);
-        if ( (! empty($this->username)) && (!empty($this->token)) ) {
-            $header = array('auth' => array($this->username, $this->token));
-            $response = $this->client->request('GET', 'repos/modxcms/revolution/tags', $header);
-        } else {
-            $response = $this->client->request('GET', 'repos/modxcms/revolution/tags');
+
+        try {
+            if ((!empty($this->username)) && (!empty($this->token))) { // use token if set
+                $header = array('auth' => array($this->username, $this->token));
+                $response = $this->client->request('GET', 'repos/modxcms/revolution/tags', $header);
+            } else { // no token
+                $response = $this->client->request('GET', 'repos/modxcms/revolution/tags');
+            }
+        } catch (Exception $e) {
+            $msg = $this->modx->lexicon('ugm_no_version_list_from_github') . ' &mdash; ' . $e->getMessage();
+            $this->addError($msg);
+            $response = false;
         }
-        return $response->getBody();
+        return $response;
     }
 
 
     public function createVersionList($method) {
         $output = '';
         $versions = $this->getJSONFromGitHub($method);
-        $versions = $this->upgrade->finalizeVersionArray($versions);
+        if ($versions === false) {
+            $output = false;
+        } else {
+            $versions = $this->upgrade->finalizeVersionArray($versions->getBody());
 
-        $itemGrid = array();
-        foreach ($versions as $ver => $item) {
-            $itemGrid[$item['tree']][$ver] = $item;
-        }
-        $i = 0;
-        $header = $this->modx->lexicon('ugm_choose_version');
-        foreach ($itemGrid as $tree => $item) {
-            $output .= "\n" . '<div class="column">';
+            $itemGrid = array();
+            foreach ($versions as $ver => $item) {
+                $itemGrid[$item['tree']][$ver] = $item;
+            }
+            $i = 0;
+            $header = $this->modx->lexicon('ugm_choose_version');
+            foreach ($itemGrid as $tree => $item) {
+                $output .= "\n" . '<div class="column">';
 
-            $output .= "\n" . '<label class="ugm_version_header"><span>' . $header . '</span></label>';
+                $output .= "\n" . '<label class="ugm_version_header"><span>' . $header . '</span></label>';
 
-            foreach ($item as $version => $itemInfo) {
-                $selected = $itemInfo['selected'] ? ' checked' : '';
-                $current = $itemInfo['current'] ? ' &nbsp;&nbsp;(' . '[[%ugm_current_version_indicator]]' . ')' : '';
-                $i = 0;
-                $output .= <<<EOD
+                foreach ($item as $version => $itemInfo) {
+                    $selected = $itemInfo['selected'] ? ' checked' : '';
+                    $current = $itemInfo['current'] ? ' &nbsp;&nbsp;(' . '[[%ugm_current_version_indicator]]' . ')' : '';
+                    $i = 0;
+                    $output .= <<<EOD
                     \n<label><input type="radio"{$selected} name="modx" value="$version">
                     <span>{$itemInfo['name']} $current</span>
                     </label>
 EOD;
-            $i++;
-            } // end inner foreach loop
-        } // end outer foreach loop
-        return $output . "\n</div>" ;
+                    $i++;
+                } // end inner foreach loop
+            } // end outer foreach loop
+            $output .= "\n</div>";
+        }
+        return $output;
     }
 
     public function process() {
-
+        /** @var $o GuzzleHttp\Psr7\request */
         $o = $this->createVersionList($this->method);
-
-        /* perform action here */
-
-        //  $o = '<h3>Version List</h3>';
-
-
-        return $this->success($o);
+        if ($o !== false) {
+            return $this->success($o);
+        } else {
+            return $this->failure('<p style="color:red">' . implode("<br>", $this->getErrors()) . '</p>');
+        }
 
     }
 }
