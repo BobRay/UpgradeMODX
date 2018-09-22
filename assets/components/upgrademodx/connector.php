df96a0f9c7f6a3d265ca1c38e6d0bd5d6c99682f
bobray <bobray@softville.com>
2018-09-22 16:14:07 -0500

    Fix bug with including config.core.php

--- assets/components/upgrademodx/connector.php
+++ assets/components/upgrademodx/connector.php
@@ -22,8 +22,9 @@
 */
 /* @var $modx modX */
 
-// include_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
+@include_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
 if (! defined('MODX_CORE_PATH')) {
+   /* For development environment */
    require_once dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))) . '/config.core.php';
 }
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
