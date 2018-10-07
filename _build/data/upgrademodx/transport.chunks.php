3182c55b95fcd866bf5204dc4bd5c867659154c8
bobray <bobray@softville.com>
2018-10-07 00:51:55 -0500

    Remove scriptsource chunk from working directory

--- _build/data/upgrademodx/transport.chunks.php
+++ _build/data/upgrademodx/transport.chunks.php
@@ -29,24 +29,12 @@ $chunks[1] = $modx->newObject('modChunk');
 $chunks[1]->fromArray(array (
   'id' => 1,
   'property_preprocess' => false,
-  'name' => 'UpgradeMODXSnippetScriptSource',
-  'description' => 'Source for MODX Upgrade script. Used internally',
-  'properties' => 
-  array (
-  ),
-), '', true, true);
-$chunks[1]->setContent(file_get_contents($sources['source_core'] . '/elements/chunks/upgrademodxsnippetscriptsource.chunk.php'));
-
-$chunks[2] = $modx->newObject('modChunk');
-$chunks[2]->fromArray(array (
-  'id' => 2,
-  'property_preprocess' => false,
   'name' => 'UpgradeMODXTpl',
   'description' => 'Tpl chunk for alert widget',
   'properties' => 
   array (
   ),
 ), '', true, true);
-$chunks[2]->setContent(file_get_contents($sources['source_core'] . '/elements/chunks/upgrademodxtpl.chunk.html'));
+$chunks[1]->setContent(file_get_contents($sources['source_core'] . '/elements/chunks/upgrademodxtpl.chunk.html'));
 
 return $chunks;
