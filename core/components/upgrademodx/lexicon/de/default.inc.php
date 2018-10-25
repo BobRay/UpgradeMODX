25361e36ebb84b0cede5ebaad54fe9bd2d85496d
bobray <bobray@softville.com>
2018-10-25 15:24:09 -0500

    Update German translation

--- core/components/upgrademodx/lexicon/de/default.inc.php
+++ core/components/upgrademodx/lexicon/de/default.inc.php
@@ -52,43 +52,43 @@ $_lang['ugm_upgrade_modx'] = 'Aktualisieren Sie MODX';
 $_lang['ugm_json_decode_failed'] = 'JSON decode für Versionsdaten von Github in upgradeAvailable() fehlgeschlagen';
 $_lang['ugm_no_curl_no_fopen'] = 'Weder allow_url_fopen noch cURL können zur Überprüfung von Updates verwendet werden';
 
-$_lang['ugm_no_version_list_from_github'] = 'Could not get version list from GitHub';
-$_lang['ugm_no_such_version'] = 'Requested version does not exist';
+$_lang['ugm_no_version_list_from_github'] = 'Konnte Versionsliste von GitHub nicht laden';
+$_lang['ugm_no_such_version'] = 'Angeforderte Version existiert nicht';
 
 /* Used in upgrademodx.class.php */
 $_lang['failed'] = 'fehlgeschlagen';
 
-$_lang['ugm_missing_versionlist'] = "Missing 'versionlist' file; try reloading the dashboard page";
-$_lang['ugm_cannot_read_directory'] = 'Unable to read directory contents or directory is empty';
-$_lang['ugm_unknown_error_reading_temp'] = 'Unknown error reading /temp directory';
-$_lang['no_method_enabled'] = 'Cannot download the files - neither cURL nor allow_url_fopen is enabled on this server.';
-$_lang['ugm_cannot_read_config_core_php'] = 'Could not read config.core.php';
-$_lang['ugm_cannot_read_main_config'] = 'Cannot Read main config file';
-$_lang['ugm_could_not_find_cacert'] = 'Could not find cacert.pem';
-$_lang['ugm_could_not_write_progress'] = 'Could not write to ugmprogress file';
-$_lang['ugm_file'] = 'File';
-$_lang['ugm_is_empty_download_failed'] = 'is empty -- download failed';
-$_lang['ugm_unable_to_create_directory'] = 'Unable to create directory';
-$_lang['ugm_unable_to_read_ugmtemp'] = 'Unable to read from /ugmtemp directory';
-$_lang['ugm_file_copy_failed'] = 'File Copy Failed';
-$_lang['ugm_begin_upgrade'] = 'Begin Upgrade';
-$_lang['ugm_starting_upgrade'] = 'Starting Upgrade';
-$_lang['ugm_downloading_files'] = 'Downloading Files';
-$_lang['ugm_unzipping_files'] = 'Unzipping Files';
-$_lang['ugm_copying_files'] = 'Copying Files';
-$_lang['ugm_preparing_setup'] = 'Preparing Setup';
-$_lang['ugm_launching_setup'] = 'Launching Setup';
-$_lang['ugm_finished'] = 'Finished';
-$_lang['ugm_get_major_versions'] = '<b>Important:</b> It is <i>strongly</i> recommended that you install all of the versions ending in .0 between your version and the current version of MODX.</p>';
-$_lang['ugm_current_version_indicator'] = 'current version';
-$_lang['ugm_using'] = 'Using';
-$_lang['ugm_choose_version'] = 'Choose MODX Version for Upgrade';
-$_lang['ugm_updating_modx_files'] = 'Updating MODX Files';
-$_lang['ugm_originally_created_by'] = 'Originally created by';
-$_lang['ugm_modified_for_revolution_by'] = 'Modified for Revolution only by';
-$_lang['ugm_modified_for_upgrade_by'] = 'Modified for Upgrade only with dashboard widget by';
-$_lang['ugm_original_design_by'] = 'Original Design By';
-$_lang['ugm_back_to_manager'] = 'Back to Manager';
+$_lang['ugm_missing_versionlist'] = "Fehlende 'versionlist' Datei; laden Sie die Dashboard-Seite neu";
+$_lang['ugm_cannot_read_directory'] = 'Ordner-Inhalte können nicht gelesen werden oder Ordner ist leer';
+$_lang['ugm_unknown_error_reading_temp'] = 'Unbekannter Fehler beim Lesen des /temp Verzeichnisses';
+$_lang['no_method_enabled'] = 'Dateien können nicht heruntergeladen werden - weder cURL noch allow_url_fopen sind auf dem Server aktiv.';
+$_lang['ugm_cannot_read_config_core_php'] = 'Konnte config.core.php nicht lesen';
+$_lang['ugm_cannot_read_main_config'] = 'Konnte Haupt-Konfigurationsdatei nicht lesen';
+$_lang['ugm_could_not_find_cacert'] = 'Konnte cacert.pem nicht finden';
+$_lang['ugm_could_not_write_progress'] = 'Konnte nicht in ugmprogress Datei schreiben';
+$_lang['ugm_file'] = 'Datei';
+$_lang['ugm_is_empty_download_failed'] = 'ist leer -- Download fehlgeschlagen';
+$_lang['ugm_unable_to_create_directory'] = 'Erstellen des Verzeichnisses fehlgeschlagen';
+$_lang['ugm_unable_to_read_ugmtemp'] = 'Konnte nicht aus /ugmtemp Verzeichnis lesen';
+$_lang['ugm_file_copy_failed'] = 'Dateikopie fehlgeschlagen';
+$_lang['ugm_begin_upgrade'] = 'Beginne Upgrade';
+$_lang['ugm_starting_upgrade'] = 'Starte Upgrade';
+$_lang['ugm_downloading_files'] = 'Dateien werden heruntergeladen';
+$_lang['ugm_unzipping_files'] = 'Dateien werden entpackt';
+$_lang['ugm_copying_files'] = 'Dateien werden kopiert';
+$_lang['ugm_preparing_setup'] = 'Setup wird vorbereitet';
+$_lang['ugm_launching_setup'] = 'Setup wird gestartet';
+$_lang['ugm_finished'] = 'Erledigt';
+$_lang['ugm_get_major_versions'] = '<b>Wichtig:</b> Es wird <i>stärkstens</i> empfohlen, dass Sie alle Versionen, die auf .0 enden, zwischen ihrer Version und der aktuellen Version von MODX installieren.</p>';
+$_lang['ugm_current_version_indicator'] = 'aktuelle Version';
+$_lang['ugm_using'] = 'Verwendung von';
+$_lang['ugm_choose_version'] = 'Wählen Sie die MODX Version für das Upgrade';
+$_lang['ugm_updating_modx_files'] = 'Aktualisiere MODX Dateien';
+$_lang['ugm_originally_created_by'] = 'Ursprünglich erstellt von';
+$_lang['ugm_modified_for_revolution_by'] = 'Modifiziert für Revolution einzigst von';
+$_lang['ugm_modified_for_upgrade_by'] = 'Modifiziert zum reinen Upgrade-Skript mit Dashboard-Widget von';
+$_lang['ugm_original_design_by'] = 'Original Design von';
+$_lang['ugm_back_to_manager'] = 'Zurück zum Manager';
 
 /* Used in System Settings */
 $_lang['setting_ugm_github_token_desc'] = 'Github token - verfügbar von ihrem GitHub Profil';
@@ -107,41 +107,41 @@ $_lang['setting_ugm_language_desc'] = 'Zwei-Zeichen Sprachcode (ISO 639-1) für 
 $_lang['setting_ugm_force_pcl_zip_desc'] = 'Erzwingen der Nutzung von PclZip anstatt von ZipArchive';
 $_lang['setting_ugm_ssl_verify_peer_desc'] = 'Zur Sicherheit, cURL die Identität des Servers verifizieren lassen';
 
-$_lang['setting_ugm_file_version'] = 'File Version';
-$_lang['setting_ugm_file_version_desc'] = 'Version when versionlist file was last updated. Set automatically -- do not edit!';
-$_lang['setting_ugm_force_pcl_zip'] = 'Force PclZip';
-$_lang['setting_ugm_version_list_path'] = 'Versionlist path';
-$_lang['setting_ugm_github_timeout'] = 'GitHub Timeout';
-$_lang['setting_ugm_modx_timeout'] = 'MODX Timeout';
-$_lang['setting_ugm_versionlist_api_url'] = 'Version List API URL';
-$_lang['setting_ugm_versionlist_api_url_desc'] = 'URL of API to get version list from; default: //api.github.com/repos/modxcms/revolution/tags';
-$_lang['setting_ugm_temp_dir'] = 'Temp directory';
-$_lang['setting_ugm_temp_dir_desc'] = 'Path to the directory used for temporary storage for downloading and unzipping files; Must be writable; default:{base_path}ugmtemp/';
-$_lang['ugm_download_failed'] = 'Download failed';
-$_lang['setting_ugm_cert_path'] = 'Cert Path';
-$_lang['setting_ugm_cert_path_desc'] = 'Path to SSL cert file in .pem format; rarely necessary';
+$_lang['setting_ugm_file_version'] = 'Datei Version';
+$_lang['setting_ugm_file_version_desc'] = 'Version, als die Versionslistendatei zuletzt aktualisiert wurde. Wird automatisch gesetzt -- nicht editieren!';
+$_lang['setting_ugm_force_pcl_zip'] = 'Erzwinge PclZip';
+$_lang['setting_ugm_version_list_path'] = 'Versionlist Pfad';
+$_lang['setting_ugm_github_timeout'] = 'GitHub Zeitüberschreitung';
+$_lang['setting_ugm_modx_timeout'] = 'MODX Zeitüberschreitung';
+$_lang['setting_ugm_versionlist_api_url'] = 'Versionslisten API-URL';
+$_lang['setting_ugm_versionlist_api_url_desc'] = 'URL der API für Versionsliste; Standard: //api.github.com/repos/modxcms/revolution/tags';
+$_lang['setting_ugm_temp_dir'] = 'Temp Verzeichnis';
+$_lang['setting_ugm_temp_dir_desc'] = 'Pfad zu dem Verzeichnis, das für den temporären Speicher zum Herunterladen und Entpacken von Dateien verwendet wird; Muss beschreibbar sein; Standard: {base_path}ugmtemp/';
+$_lang['ugm_download_failed'] = 'Download fehlgeschlagen';
+$_lang['setting_ugm_cert_path'] = 'Cert Pfad';
+$_lang['setting_ugm_cert_path_desc'] = 'Pfad zu SSL-Zertifikat Datei im .pem Format (selten benötigt)';
 
 /* System Setting Area strings */
 $_lang['Download'] = 'Download';
-$_lang['Form'] = 'Form';
+$_lang['Form'] = 'Formular';
 $_lang['GitHub'] = 'GitHub';
-$_lang['Security'] = 'Security';
+$_lang['Security'] = 'Sicherheit';
 $_lang['Widget'] = 'Widget';
 
 /* Used in copyfiles.class.php */
-$_lang['ugm_copied'] = 'Copied';
-$_lang['ugm_to'] = 'to';
-$_lang['ugm_files_copied'] = 'Objects copied';
+$_lang['ugm_copied'] = 'Kopiert';
+$_lang['ugm_to'] = 'nach';
+$_lang['ugm_files_copied'] = 'Dateien kopiert';
 
 /* Used in downloadfiles.class.php */
-$_lang['ugm_downloaded'] = 'Downloaded';
-$_lang['ugm_download_failed'] = 'Download failed';
+$_lang['ugm_downloaded'] = 'Heruntergeladen';
+$_lang['ugm_download_failed'] = 'Download fehlgeschlagen';
 
 /* Used in preparesetup.class.php */
-$_lang['ugm_no_root_config_core'] = 'Could not find root config.core.php';
-$_lang['ugm_setup_prepared'] = 'Setup prepared';
-$_lang['ugm_could_not_write'] = 'Could not write';
+$_lang['ugm_no_root_config_core'] = 'Konnte config.core.php im Root nicht finden';
+$_lang['ugm_setup_prepared'] = 'Installation vorbereitet';
+$_lang['ugm_could_not_write'] = 'Konnte nicht schreiben';
 
 /* Used in cleanup.class.php */
-$_lang['ugm_deleting_temp_files'] = 'Cleaning Up';
-$_lang['ugm_temp_files_deleted'] = 'Cleanup Completed (temporary files removed)';
+$_lang['ugm_deleting_temp_files'] = 'Am Aufräumen';
+$_lang['ugm_temp_files_deleted'] = 'Aufräumen komplett (temporäre Dateien gelöscht)';
