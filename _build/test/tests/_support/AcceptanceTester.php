aaacc4a20c5b7c2ab9117557af6886e2dc6c28a2
bobray <bobray@softville.com>
2019-01-27 23:53:16 -0500

    Prevent some method not found warnings

--- _build/test/tests/_support/AcceptanceTester.php
+++ _build/test/tests/_support/AcceptanceTester.php
@@ -13,6 +13,18 @@
  * @method void lookForwardTo($achieveValue)
  * @method void comment($description)
  * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ * @method amOnPage($page)
+ * @method see($text, $selector = null)
+ * @method scrollTo($selector, $offsetX = null, $offsetY = null)
+ * @method grabAttributeFrom($cssOrXpath, $attribute)
+ * @method wait($timeout)
+ * @method waitForText($text, $timeout = null, $selector = null)
+ * @method waitForElementVisible($element, $timeout = null)
+ * @method waitForElement($element, $timeout = null)
+ * @method clickWithLeftButton($cssOrXPath = null, $offsetX = null, $offsetY = null)
+ * @method clickWithRightButton($cssOrXPath = null, $offsetX = null, $offsetY = null)
+ * @method click($link, $context = null)
+ *
  *
  * @SuppressWarnings(PHPMD)
 */
