4e8eb3f2360cb6f2531cc1023254b6464cd3f4c7
bobray <bobray@softville.com>
2026-05-11 00:04:05 -0500

    Version to 2.3.7-pl -- released

--- _build/config/upgrademodx.config.php
+++ _build/config/upgrademodx.config.php
@@ -18,14 +18,14 @@ $components = array(
     'packageName' => 'UpgradeMODX',  /* No spaces, no dashes */
     'packageNameLower' => $packageNameLower,
     'packageDescription' => 'UpgradeMODX project extra',
-    'version' => '2.3.6',
+    'version' => '2.3.7',
     'release' => 'pl',
     'author' => 'Bob Ray',
     'email' => '<https://bobsguides.com>',
     'authorUrl' => 'https://bobsguides.com',
     'authorSiteName' => "Bob's Guides",
     'packageDocumentationUrl' => 'https://bobsguides.com/upgrademodx-tutorial.html',
-    'copyright' => '2015-2025',
+    'copyright' => '2015-2026',
 
     /* no need to edit this except to change format */
     'createdon' => $d,
